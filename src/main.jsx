a78b78f306cdeac1f374c5938fb5978d7779406e
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-03-02 20:51:45 +0100

    I made some additional settings to deploy the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import { App } from "./components";
 import "modern-normalize/modern-normalize.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
